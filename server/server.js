0c7bd03a9bee25770eb58a11d9204375882c87dd
David Park <dspark410@gmail.com>
2021-08-07 12:22:04 -0700

    adds graphql boilerplate

--- server/server.js
+++ server/server.js
@@ -1,15 +1,27 @@
 const express = require('express')
+const { ApolloServer } = require('apollo-server-express')
+const { typeDefs } = require('./schemas/typeDefs')
+const { resolvers } = require('./schemas/resolvers')
 require('dotenv').config()
-
 const db = require('./config/connection')
 
-const app = express()
 const PORT = process.env.PORT || 5000
+const app = express()
+
+let server
+const startApolloServer = async () => {
+  server = new ApolloServer({ typeDefs, resolvers })
+  await server.start()
+  server.applyMiddleware({ app })
+}
+
+startApolloServer()
 
 app.get('/', (req, res) => res.send('server running..'))
 
 db.once('open', () => {
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`)
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`)
   })
 })
