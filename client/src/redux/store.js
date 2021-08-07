bb57225fbd88ba6379b19b4fe48dd1a6d1ad8124
David Park <dspark410@gmail.com>
2021-08-06 22:09:17 -0700

    adds client boilerplate

---
+++ client/src/redux/store.js
@@ -0,0 +1,12 @@
+import { createStore, combineReducers, applyMiddleware } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import thunk from 'redux-thunk'
+
+const reducers = combineReducers({})
+
+const middleware = [thunk]
+
+export const store = createStore(
+  reducers,
+  composeWithDevTools(applyMiddleware(...middleware))
+)
